8bf4c58d3fdfcf82a980115331438ae9e3528057
Aditya Pradip Desai <apd10@jaya.cs.rice.edu>
2020-06-10 18:38:05 -0500

    minor script fixes

--- topk/setup_datasets.sh
+++ topk/setup_datasets.sh
@@ -1,5 +1,6 @@
 OLD=$PWD # you should be in topK directory to run this
 mkdir -p $PWD/../url/
+mkdir -p $PWD/record
 cd $PWD/../url/
 wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/url_combined.bz2
 bzip2 -d url_combined.bz2
